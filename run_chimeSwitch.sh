e3584dd50d67c3176b865be827906ab8b41a7ff1
niscode <nishimura.s405@gmail.com>
2023-01-27 13:25:49 +0900

    chimeSwitch updated

--- run_chimeSwitch.sh
+++ run_chimeSwitch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 ### for Jetson
 
-count=`ps -ef | grep sudo | grep -v grep | wc -l`
 exp=1
 
 while true
 do
   echo "Please wait for 10 sec ..."
   sleep 10
+  count=`ps -ef | grep sudo | grep -v grep | wc -l`
   # sudoプロセスを監視
   if [ $count = 0 ]; then
     echo "[-]chimeSwitch Process Down  -----"
@@ -18,14 +18,14 @@ do
     echo $((exp ++))
 
   else
-    if [ $exp = 2 ] || [ $((exp / 10)) = 0 ]; then
-      # 初回起動の停止時と100秒に1回、プロセスを強制キル
+    if [ $exp = 2 ] || [ $((exp / 2)) = 0 ]; then
+      # 初回起動の停止時と20秒に1回、プロセスを強制キル
       pid=`ps -ax | grep sudo | grep keyboard | awk '{ print $1 }'`
-      echo 'capf' | sudo -S kill -9 $pid``
+      echo 'capf' | sudo -S kill -9 $pid
     else
-      echo "[+]chimeSwitch Process OK    +++++"
       sleep 10
     fi
+    echo "[-]chimeSwitch Process Quited    -----"
     echo $((exp ++))
   fi
 
