52769e28894719748ac6f491d865cd78246e3e59
niscode <nishimura.s405@gmail.com>
2023-02-06 15:08:12 +0900

    launch_bridgeを追加

--- launch_client.sh
+++ launch_client.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-python3 ~/pythonscript/webagent/webagent.py "https://ignis2.ca-platform.org/api/login" "CA003" "CA003" "wss://ignis2-websocket.ca-platform.org" "192.168.10.4" 5001
+# python3 ~/pythonscript/webagent/webagent.py "https://ignis2.ca-platform.org/api/login" "CA003" "CA003" "wss://ignis2-websocket.ca-platform.org" "192.168.10.4" 5001
 
 ### for Linux --- just TCP socket connection
-# python3 ~/pythonscript/client.py CA001 ignis2-sock.ca-platform.org 11001 192.168.10.2 5001
+python3 ~/pythonscript/client.py CA001 ignis2-sock.ca-platform.org 11001 192.168.10.2 5001
 
 
 ### for macOS / windows
