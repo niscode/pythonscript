76a2803a6a884cb6c28c083c3ca8e00395e1495f
niscode <nishimura.s405@gmail.com>
2023-03-02 12:08:19 +0900

    起動時に動作するよう設定

--- autostart/Readme.md
+++ autostart/Readme.md
@@ -1,7 +1,11 @@
 ## Startup Applicationsで実行するスクリプト
-### Ubuntu 20.04向け
+### Ubuntu向け
 
-以下にファイルを配置
-~/.config/autostart/
+▼ 以下のディレクトリにファイルを配置
 
-get_output.sh は不要
+`~/.config/autostart/`
+
+- auto_chrome.desktop
+- auto_client.desktop
+- set_input.desktop
+- set_output.desktop
--- launch_ws.sh
+++ launch_ws.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # 引数の指定: <login url> <id> <passwd> <websocket url> <robot-addr> <robot-port> <targetid>
 
-python3 ~/pythonscript/webagent.py "https://ignis2.ca-platform.org/api/login" "CA003" "CA003" "wss://ignis2-websocket.ca-platform.org" "10.186.42.103" 5001 'OP002SA'
+python3 ~/pythonscript/webagent.py "https://ignis2.ca-platform.org/api/login" "CA003" "CA003" "wss://ignis2-websocket.ca-platform.org" "192.168.10.4" 5001 'OP002SA'
